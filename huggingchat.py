a89d289d795b047cacd232ac9d59afa55f08fcba
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2023-07-26 13:02:03 +0500

    Added some cool things

---
+++ huggingchat.py
@@ -0,0 +1,145 @@
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait  #..............
+from selenium.webdriver.support import expected_conditions as EC    #................
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+
+
+class HuggingChatScraperBot:
+    def __init__(self, headless_mode=False) -> None:
+        self.headless_mode = headless_mode
+        self.driver = self.gen_driver()
+        self.prompt = ""
+        self.response_list = []
+
+    def gen_driver(self):
+        try:
+            chrome_options = uc.ChromeOptions()
+            if self.headless_mode:
+                # chrome_options.headless = True
+                chrome_options.add_argument('--headless=new')
+            driver = uc.Chrome(options=chrome_options)
+            return driver
+        except Exception as e:
+            print("Error in Driver: ",e)
+
+    def prosecutor(self, scenario):
+        self.driver.get("https://huggingface.co/chat/")
+        self.prompt = """
+        You are a story teller.
+        Please generate a short story based on a simple narrative, the story should not be more than 200 words.
+
+        Following are given two separate contexts, choose either one or maybe both to construct a perfect story.
+
+        CONTEXT 1: {}
+        CONTEXT 2: {}
+        STORY:
+        """.format(scenario[0],scenario[1])
+        print("Prompt: {}".format(self.prompt))
+
+        self.enter_prompt()
+        
+        text_response = self.get_response()
+
+        self.response_list.extend([self.prompt, text_response, self.prompt+text_response])
+
+        return self.response_list
+
+    def enter_prompt(self):
+        # Send Keys
+        self.send_keys_manager(
+            "Entered Text", 
+            '//*[@id="app"]/div[1]/div/div[2]/form/div/div/textarea',
+            self.prompt
+            )
+        # Click on Send Prompt Button
+        self.click_manager(
+            "Enter Text BTN",
+            '//*[@id="app"]/div[1]/div/div[2]/form/div/button'
+        )
+
+    def get_response(self):
+        text_response = self.get_text_manager(
+            "Get Text",
+            '//*[@id="app"]/div[1]/nav[3]/div[2]/a[1]/div',
+            '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[1]/div'
+        )
+        return text_response
+
+    def wait_manager(self, identifier, t, mode=By.XPATH):
+        i = 1
+        while True:
+            try:
+                # WebDriverWait(self.driver, t).until(EC.visibility_of_element_located((By.CLASS_NAME, identifier)))
+                element = self.driver.find_element(mode, identifier)
+                if element:
+                    print("[{}]Wait Manager [Success]: Element Detected no need to wait for {} seconds".format(i,t))
+                    break
+                else:
+                    print("[{}]Wait Manager [Active]: Element not Detected! Waiting for {} seconds".format(i,t))
+                    time.sleep(t)
+                # WebDriverWait(self.driver, t).until(EC.presence_of_element_located((By.CSS_SELECTOR, identifier)))
+            except Exception as e:
+                print("[{}]Wait Manager [Error]: {}\nNow waiting for {} seconds!".format(i,str(e).split('\n')[0],t))
+                time.sleep(t)
+            i+=1
+
+    def click_manager(self, name, identifier, mode=By.XPATH):
+        t = 3
+        i=1
+        while True:
+            try:
+                self.driver.find_element(mode, identifier).click()
+                print("[{}]Click Manager [Success]: {} clicked successfully!".format(i,name))
+                break
+            except Exception as e:
+                print("[{}]Click Manager [Error]:{}\n[{}]Click Manager [Active]: Activating Wait Manager".format(i,str(e).split('\n')[0],i))
+                # time.sleep(t)
+                self.wait_manager(identifier, t, mode)
+            i+=1
+
+    def get_text_manager(self, name, tag_identifier, text_identifier, mode=By.XPATH):
+        t = 3
+        i=1
+        while True:
+            try:
+                generated_text = self.driver.find_element(mode, text_identifier).text
+                chat_tag = self.driver.find_element(mode, tag_identifier).text
+                if chat_tag.find("Untitled") == -1:
+                    print("[{}]Get Text Manager [Success]: {} content generated successfully!".format(i,name))
+                    break
+                else:
+                    print("[{}]Get Text Manager [Waiting]: {} waiting for content generation".format(i,name))
+            except Exception as e:
+                print("[{}]Get Text Manager [Error]:{}\n[{}]Get Text Manager [Active]: Activating Wait Manager".format(i,str(e).split('\n')[0],i))
+                # time.sleep(t)
+                self.wait_manager(text_identifier, t, mode)
+            i+=1
+        return generated_text
+
+    def send_keys_manager(self, name, identifier, keys, mode=By.XPATH):
+        t = 3
+        i=1
+        while True:
+            try:
+                for part in keys.split('\n'):
+                    self.driver.find_element(mode, identifier).send_keys(part)
+                    ActionChains(self.driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.SHIFT).key_up(Keys.ENTER).perform()
+                print("[{}]Send Keys Manager [Success]: {} keys sent successfully!".format(i,name))
+                break
+            except Exception as e:
+                print("[{}]Send Keys Manager [Error]:{}\n[{}]Send Keys Manager [Active]: Activating Wait Manager".format(i,str(e).split('\n')[0],i))
+                self.wait_manager(identifier, t, mode)
+            i+=1
+
+
+if __name__ == '__main__':
+    scenario = "two bulls fighiting each other"
+    agent = HuggingChatScraperBot(headless_mode=True)
+    response = agent.prosecutor([scenario,scenario])
+    print(response[2])
+    os.system("pause")
+    
--- main.py
+++ main.py
@@ -2,39 +2,194 @@ from dotenv import find_dotenv, load_dotenv
 from transformers import pipeline
 from langchain import PromptTemplate, LLMChain, OpenAI
 from langchain.chat_models import ChatOpenAI
+from huggingchat import HuggingChatScraperBot
+from moviepy.editor import *
+from pydub import AudioSegment
+from PIL import Image
+import requests
+import os
 
 load_dotenv(find_dotenv())
+HUGGINFACEHUB_API_TOKEN = os.getenv("HUGGINFACEHUB_API_TOKEN")
 
-# img2text
-def img2text(url):
-    # Use a pipeline as a high-level helper
-    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+class ImageToStory:
+    def __init__(self):
+        self.scenerio = []
+        self.story = ""
+
+    def driver(self, img_url, export_url="export"):
+        # Calll a Directory Handler
+        self.directory_handler(export_url)
+
+        # Image Scenario Captioning
+        self.scenerio = [self.img2text_blip(img_url), self.img2text_coco(img_url)]
+        print(self.scenerio)
+
+        # Passing to LLM for Story Generation from given Scenario
+        self.story = self.generate_story(self.scenerio)
+
+        # Story to Speech Synthesis
+        audio_url = self.text2speech(self.story, export_url)
+
+        # Compile all of it into a movie clip
+        self.convert2movie(img_url, audio_url, export_url)
+
+    def directory_handler(self, folder_path):
+        # Check if the folder exists
+        if os.path.exists(folder_path):
+            # If it exists, delete it
+            try:
+                os.rmdir(folder_path)  # Remove the folder (only works if it's empty)
+            except OSError:
+                # If the folder is not empty, remove it with all its contents
+                import shutil
+                shutil.rmtree(folder_path)
+            print(f"Deleted existing folder: {folder_path}")
+
+        # Create a new folder
+        try:
+            os.makedirs(folder_path)
+            print(f"Created new folder: {folder_path}")
+        except OSError:
+            print(f"Failed to create the folder: {folder_path}")
     
-    text = image_to_text(url)[0]["generated_text"]
+    def img2text_blip(self, img_url):
+        print("Converting Image to Text [BLIP]")
+        API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base"
+        headers = {"Authorization": "Bearer {}".format(HUGGINFACEHUB_API_TOKEN)}
+        with open(img_url, "rb") as f:
+            data = f.read()
+        response = requests.post(API_URL, headers=headers, data=data)
+        return response.json()[0]["generated_text"]
+    
+    def img2text_coco(self, img_url):
+        print("Converting Image to Text [COCO]")
+        API_URL = "https://api-inference.huggingface.co/models/microsoft/git-large-coco"
+        headers = {"Authorization": "Bearer {}".format(HUGGINFACEHUB_API_TOKEN)}
+        with open(img_url, "rb") as f:
+            data = f.read()
+        response = requests.post(API_URL, headers=headers, data=data)
+        return response.json()[0]["generated_text"]
 
-    print(text)
-    return text
+    # LLM
+    def generate_story(self, scenario):
+        agent = HuggingChatScraperBot(True)
+        response = agent.prosecutor(scenario=scenario)
+        print(response[2])
+        return response[1]
+    
+    # Text to speech
+    def text2speech(self, story, export_url):
+        API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+        headers = {"Authorization": "Bearer {}".format(HUGGINFACEHUB_API_TOKEN)}
+        payloads = {
+            "inputs": story
+        }
+        print("Converting Story to Speech..............")
+        response = requests.post(API_URL, headers=headers, json=payloads)
+        audio_path = export_url+'/audio.flac'
+        with open(audio_path, 'wb') as file:
+            file.write(response.content)
+        return audio_path
 
-# LLM
-def generate_story(scenario):
-    template = """
-    You are a story teller;
-    Please generate a short story based on a simple narrative, the story should not be more than 200 words;
+    def convert2movie(self, image_path, audio_path, output_path):
+        print("Compiling into Movie.............")
+        # Load the image
+        image_clip = ImageClip(image_path)
 
-    CONTEXT: {scenario}
-    STORY:
-    """
-    prompt = PromptTemplate(template=template, input_variables=["scenario"])
+        # Load the audio
+        if (nilter := audio_path.split("."))[-1] == "flac":
+            self.convert_flac_to_mp3(audio_path, nilter[0] + ".mp3", "320k")
+            audio_path = nilter[0] + ".mp3"
+        audio_clip = AudioFileClip(audio_path)
 
-    story_llm = LLMChain(llm=OpenAI(model_name="gpt-3.5-turbo", temperature=1), prompt=prompt, verbose=True)
+        # Set the duration of the video to match the audio duration
+        image_clip = image_clip.set_duration(audio_clip.duration)
 
-    story = story_llm.predict(scenario=scenario)
+        # Resize the image to 1080x1920
+        target_width, target_height = 1080, 1920
+        image_clip = image_clip.resize((target_width, target_height)).set_position(('center', 'center'))
 
-    print(story)
+        # Combine the image and audio
+        video_clip = image_clip.set_audio(audio_clip)
 
-    return story
+        # Write the final video to the output path
+        video_clip.write_videofile(output_path+"/video.mp4", codec='libx264', audio_codec='aac', fps=30)
+
+
+
+
+    def convert_flac_to_mp3(self, input_path, output_path, bitrate='128k'):
+        """
+        Converts audio from FLAC format to MP3 format.
+
+        Parameters:
+            input_path (str): Path to the input FLAC audio file.
+            output_path (str): Path to save the output MP3 audio file.
+            bitrate (str): Bitrate of the output MP3 file (default: '128k').
+                        You can choose from different bitrates like '64k', '128k', '192k', etc.
+
+        Returns:
+            None
+        """
+        print("Converting flac to mp3............")
+        audio = AudioSegment.from_file(input_path, format="flac")
+        audio.export(output_path, format="mp3", bitrate=bitrate) 
+
+    def resize_image(image_path, output_path, target_width, target_height):
+        with Image.open(image_path) as img:
+            # Get the original dimensions
+            original_width, original_height = img.size
+
+            # Calculate the aspect ratios
+            original_aspect_ratio = original_width / original_height
+            target_aspect_ratio = target_width / target_height
+
+            # Determine how to resize the image
+            if original_aspect_ratio > target_aspect_ratio:
+                # Fit by width
+                new_width = target_width
+                new_height = int(new_width / original_aspect_ratio)
+            else:
+                # Fit by height
+                new_height = target_height
+                new_width = int(new_height * original_aspect_ratio)
+
+            # Resize the image
+            resized_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+
+            # Create a new blank image with the target dimensions
+            background = Image.new('RGBA', (target_width, target_height), (255, 255, 255, 0))
+
+            # Calculate the position to paste the resized image
+            paste_x = (target_width - new_width) // 2
+            paste_y = (target_height - new_height) // 2
+
+            # Paste the resized image onto the blank image
+            background.paste(resized_img, (paste_x, paste_y))
+
+            # Save the final image
+            background.save(output_path)
+
+    def generate_story_with_ChatGPT(self, scenario):
+        template = """
+        You are a story teller;
+        Please generate a short story based on a simple narrative, the story should not be more than 200 words;
+
+        CONTEXT: {scenario}
+        STORY:
+        """
+        prompt = PromptTemplate(template=template, input_variables=["scenario"])
+
+        story_llm = LLMChain(llm=OpenAI(model_name="gpt-3.5-turbo", temperature=1), prompt=prompt, verbose=True)
+
+        story = story_llm.predict(scenario=scenario)
+
+        print(story)
+
+        return story
 
 if __name__ == '__main__':
-    # scenario = img2text("test_img3.png")
-    scenario = "a gifet and a gifet"
-    story = generate_story(scenario)
+    agent = ImageToStory()
+    agent.driver("images/test_img6.jpg")
+    print("Done!")
