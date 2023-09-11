0d395b9efcdea60a55335fbffb88ae0e0bdcaf66
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2023-09-12 00:20:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -97,4 +97,5 @@ def get_video(user_id: str):
     return HTMLResponse(content=html_content)
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host="0.0.0.0")
+    # uvicorn.run(app, port=8000, host="0.0.0.0")
+    uvicorn.run(app)
